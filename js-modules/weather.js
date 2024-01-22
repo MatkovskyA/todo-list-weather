3ae64dd3ed7432c8453c0d8d9dc86466a7592423
MatkovskyA <a.matkovsky14@gmail.com>
2024-01-22 11:28:13 +0300

    добавлен стиль блоку температура

--- js-modules/weather.js
+++ js-modules/weather.js
@@ -1,12 +1,23 @@
 //weatherApi.com
-const apiKey = "c40732b25e994dbd96264614242201";
+// const apiKey = "c40732b25e994dbd96264614242201";
 
-fetch(
-  `http://api.weatherapi.com/v1/current.json?key=c40732b25e994dbd96264614242201&q=London`
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// fetch(
+//   `http://api.weatherapi.com/v1/current.json?key=c40732b25e994dbd96264614242201&q=London`
+// )
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+// получаем инпут и форму для погоды
+const weatherForm = document.querySelector(".form-weather");
+const inputLocation = document.querySelector(".weather-location");
+//слушаем событие формы и получаем значение города из инпута
+weatherForm.onsubmit = (event) => {
+  event.preventDefault();
+  let city;
+  city = inputLocation.value.trim();
+  console.log(city);
+};
