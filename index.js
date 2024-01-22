3f2f709b0843be9a1819c78e37c1992651447d61
MatkovskyA <a.matkovsky14@gmail.com>
2024-01-22 09:28:34 +0300

    Сделано добавление задач, удаление и отметка решено

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const taskAddBtn = document.querySelector(".task-btn"); // получаем кн
 const tasksList = document.querySelector(".tasks-list"); //список отображения задач
 const taskError = document.querySelector(".task-error"); //текст о валидации инпута
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+//по нажатию на enter отображаем задачу
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   getInputValue();
 });
-
-taskAddBtn.addEventListener("click", getInputValue); // при нажатии на кнопку
+// при нажатии на кнопку отображаем задачу
+taskAddBtn.addEventListener("click", getInputValue);
 
 function getInputValue() {
-  const taskValue = inputNode.value;
-  if (taskValue == "" || taskValue.length < 3) {
+  //проверяем ввод данных в инпут
+
+  const taskValue = inputNode.value.trim();
+  if (taskValue === "" || taskValue.length < 3) {
     taskError.classList.remove("hidden");
     return;
   } else {
@@ -24,20 +27,45 @@ function getInputValue() {
   const taskHTML = `
     <li class="task">
     <div class="task-text">
-        <span class="task-number">1.</span>
         <span class="task-content">${taskValue.trim()}</span>
     </div>
     <div class="task-btns">
-        <div class="task-btn-completed"></div>
-        <img src="resources/cross_mr3w4khsljuu.svg" class="task-btn-delete" alt="delete-task-icon">
+        <button class="task-btn-completed" data-action="completed"></button>
+        <button class="task-btn-delete" data-action="deleted"></button>
     </div>
   </li>
     `;
   tasksList.insertAdjacentHTML("beforeend", taskHTML);
-
   clearInputValue();
 }
 
+// удаление задачи
+function deleteTask(event) {
+  if (event.target.dataset.action === "deleted") {
+    const parentNode = event.target.closest("li");
+    parentNode.remove();
+  }
+}
+
+//слушатель для удаления задачи
+tasksList.addEventListener("click", deleteTask);
+//отмечаем выполненную задачу
+tasksList.addEventListener("click", doneTask);
+
+function doneTask(event) {
+  const liTask = event.target.closest(".task");
+  const taskTitle = liTask.querySelector(".task-content");
+  const taskBtn = liTask.querySelector(".task-btn-completed");
+  if (event.target.dataset.action === "completed") {
+    console.log(taskTitle);
+    taskTitle.classList.toggle("task-content-done");
+    taskBtn.classList.toggle("task-btn-completed-done");
+
+    // setTimeout(() => {
+    //   liTask.remove();
+    // }, 2000);
+  }
+}
 //очищаем инпут после ввода данных
 function clearInputValue() {
   inputNode.value = "";
